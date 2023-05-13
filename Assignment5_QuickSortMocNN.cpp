c294e1774d76377a689928b09f35a00e43641a91
Nha Triet <91239082+nhatriet@users.noreply.github.com>
2023-05-14 04:30:05 +0700

    assignment 5

---
+++ Assignment5_QuickSortMocNN.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>      // thu vien de khoi tao tham so cho ham rand()
+#define swap(type, a, b) {type temp = a; a = b; b = temp; } // hang hoan vi
+ 
+void quickSort(int *a, int l, int r)
+{
+    srand(time(NULL));  //khoi tao tham so ham rand()
+    int key = a[l + rand() % (r-l+1)];  //lay khoa la gia tri ngau nhien tu a[l] -> a[r]
+    //int key = a[(l+r)/2];
+    int i = l, j = r;
+ 
+    while(i <= j)
+    {
+        while(a[i] < key) i++;       // tim phan tu ben trai ma >=key
+        while(a[j] > key) j--;       // tim phan tu ben trai ma <=key
+        if(i <= j)
+        {
+            if (i < j)
+                swap(int, a[i], a[j]);  // doi cho 2 phan tu kieu int a[i], a[j].
+            i++;
+            j--;
+        }
+    }
+    //bay gio ta co 1 mang : a[l]....a[j]..a[i]...a[r]
+    if (l < j) quickSort(a, l, j);   // lam lai voi mang a[l]....a[j]
+    if (i < r) quickSort(a, i, r); // lam lai voi mang a[i]....a[r]
+}
+ 
+int main ()
+{
+	int i, arr[] = { 2, 8, 7, 1, 3, 5, 6, 4  };
+    int n = 8;
+    printf("\n\nArray befor sort: ");
+    for (i=0; i<n; i++)
+        printf ("%d ", arr[i]);
+    quickSort(arr, 0, n-1);
+ 
+    printf("\n\nArray after sort: ");
+    for (i=0; i<n; i++)
+        printf ("%d ", arr[i]);
+}
